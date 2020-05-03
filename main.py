9b5f35f3e872920491906d43f44a2c5f311c5e29
JWBN <admin@wbnk.net>
2020-05-03 20:41:34 +0100

    added experimental video functionality. combobox added allowing user to select desired format (audio or video currently).

--- main.py
+++ main.py
@@ -7,17 +7,25 @@ class MyFrame(wx.Frame):
     def __init__(self):
         super().__init__(parent=None, title='youtube-helper')
         panel = wx.Panel(self)
+        availableTypes = ["mp3", "video"]
+        userChoice = "mp3"
         my_sizer = wx.BoxSizer(wx.VERTICAL)
         self.text_ctrl = wx.TextCtrl(panel)
         my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)
         my_btn = wx.Button(panel, label='Download video')
         my_btn.Bind(wx.EVT_BUTTON, self.on_press)
         my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)
+        self.combobox = wx.ComboBox(panel, choices=availableTypes, pos=(50,50))
+        my_sizer.Add(self.combobox, 0, wx.ALL | wx.CENTER, 60)
+        self.Bind(wx.EVT_COMBOBOX, self.OnCombo)
         panel.SetSizer(my_sizer)
         self.Show()
 
+    def OnCombo(self, event):
+        print("Debug")
+
     def on_press(self, event):
-        ydl_opts = {
+        ydl_opts_audio = {
             'format': 'bestaudio/best',
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
@@ -25,19 +33,28 @@ class MyFrame(wx.Frame):
                 'preferredquality': '192',
             }]
         }
+
+        ydl_opts_video = {
+            'format': 'best'
+        }
+
         value = self.text_ctrl.GetValue()
         if not value:
             print("You didn't enter anything!")
         else:
             print("Downloading and converting. Be patient.")
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([value])
+            userChoice = self.combobox.GetValue()
+            if userChoice == "mp3":
+                with youtube_dl.YoutubeDL(ydl_opts_audio) as ydl:
+                    ydl.download([value])
+            elif userChoice == "video":
+                with youtube_dl.YoutubeDL(ydl_opts_video) as ydl:
+                    ydl.download([value])
             resp = wx.MessageBox('Download has finished, are you done?', 'Finished!', wx.YES_NO)
             if resp == wx.YES:
                 self.Destroy()
             elif resp == wx.NO:
                 return
-
 
 if __name__ == '__main__':
     app = wx.App()
