96268a25a90a99caf9a1d5e4d73e0c7cb4406ca7
JWBN <admin@wbnk.net>
2020-05-04 14:10:17 +0100

    simple status label so user knows what is going on.

--- main.py
+++ main.py
@@ -10,13 +10,16 @@ class MyFrame(wx.Frame):
         available_formats = ["mp3", "video"]
         form_sizer = wx.BoxSizer(wx.VERTICAL)
         self.text_ctrl = wx.TextCtrl(panel)
+        self.status_label = wx.StaticText(panel, label="Waiting for user input...")
+        self.status_label.Wrap(210)
+        form_sizer.Add(self.status_label, 0, wx.ALL | wx.CENTER, 5)
         form_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)
         download_button = wx.Button(panel, label='Download video')
         download_button.Bind(wx.EVT_BUTTON, self.on_press)
         form_sizer.Add(download_button, 0, wx.ALL | wx.CENTER, 5)
-        self.format_selection = wx.Choice(panel, choices=available_formats, pos=(50, 50))
+        self.format_selection = wx.Choice(panel, choices=available_formats, pos=(50, 10))
         self.format_selection.SetSelection(0)
-        form_sizer.Add(self.format_selection, 0, wx.ALL | wx.CENTER, 60)
+        form_sizer.Add(self.format_selection, 0, wx.ALL | wx.CENTER, 20)
         panel.SetSizer(form_sizer)
         self.Show()
 
@@ -39,6 +42,7 @@ class MyFrame(wx.Frame):
             wx.MessageBox('Nothing was entered in the box', 'Error', wx.OK)
         else:
             print("Downloading and converting. Be patient.")
+            self.status_label.SetLabel("Downloading")
             format_choice = self.format_selection.GetSelection()
             if format_choice == 0:
                 with youtube_dl.YoutubeDL(ydl_opts_audio) as ydl:
@@ -46,6 +50,7 @@ class MyFrame(wx.Frame):
             elif format_choice == 1:
                 with youtube_dl.YoutubeDL(ydl_opts_video) as ydl:
                     ydl.download([value])
+            self.status_label.SetLabel("Waiting for user input...")
             resp = wx.MessageBox('Download has finished, would you like to exit?', 'Download complete', wx.YES_NO)
             if resp == wx.YES:
                 self.Destroy()
