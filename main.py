062e1465cf46ae6b252e94f8aabfb0fe7b73fa16
JWBN <admin@wbnk.net>
2020-05-04 13:59:30 +0100

    variables renamed so that they are easier to understand

--- main.py
+++ main.py
@@ -7,20 +7,18 @@ class MyFrame(wx.Frame):
     def __init__(self):
         super().__init__(parent=None, title='youtube-helper')
         panel = wx.Panel(self)
-        availableTypes = ["mp3", "video"]
-        my_sizer = wx.BoxSizer(wx.VERTICAL)
+        available_formats = ["mp3", "video"]
+        form_sizer = wx.BoxSizer(wx.VERTICAL)
         self.text_ctrl = wx.TextCtrl(panel)
-        my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)
-        my_btn = wx.Button(panel, label='Download video')
-        my_btn.Bind(wx.EVT_BUTTON, self.on_press)
-        my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)
-        self.combobox = wx.Choice(panel, choices=availableTypes, pos=(50,50))
-        self.combobox.SetSelection(0)
-        my_sizer.Add(self.combobox, 0, wx.ALL | wx.CENTER, 60)
-        panel.SetSizer(my_sizer)
+        form_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)
+        download_button = wx.Button(panel, label='Download video')
+        download_button.Bind(wx.EVT_BUTTON, self.on_press)
+        form_sizer.Add(download_button, 0, wx.ALL | wx.CENTER, 5)
+        self.format_selection = wx.Choice(panel, choices=available_formats, pos=(50, 50))
+        self.format_selection.SetSelection(0)
+        form_sizer.Add(self.format_selection, 0, wx.ALL | wx.CENTER, 60)
+        panel.SetSizer(form_sizer)
         self.Show()
-
-
 
     def on_press(self, event):
         ydl_opts_audio = {
@@ -41,19 +39,19 @@ class MyFrame(wx.Frame):
             wx.MessageBox('Nothing was entered in the box', 'Error', wx.OK)
         else:
             print("Downloading and converting. Be patient.")
-            userChoice = self.combobox.GetSelection()
-            print(userChoice)
-            if userChoice == 0:
+            format_choice = self.format_selection.GetSelection()
+            if format_choice == 0:
                 with youtube_dl.YoutubeDL(ydl_opts_audio) as ydl:
                     ydl.download([value])
-            elif userChoice == 1:
+            elif format_choice == 1:
                 with youtube_dl.YoutubeDL(ydl_opts_video) as ydl:
                     ydl.download([value])
-            resp = wx.MessageBox('Download has finished, are you done?', 'Finished!', wx.YES_NO)
+            resp = wx.MessageBox('Download has finished, would you like to exit?', 'Download complete', wx.YES_NO)
             if resp == wx.YES:
                 self.Destroy()
             elif resp == wx.NO:
                 return
+
 
 if __name__ == '__main__':
     app = wx.App()
