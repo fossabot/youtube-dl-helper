09a5386becafd0d5cdda2a1d01278d44834606ae
JWBN <admin@wbnk.net>
2020-05-04 10:49:00 +0100

    remove unused functions and variables. fixed broken format selection by getting correct value.

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ class MyFrame(wx.Frame):
         super().__init__(parent=None, title='youtube-helper')
         panel = wx.Panel(self)
         availableTypes = ["mp3", "video"]
-        userChoice = "mp3"
         my_sizer = wx.BoxSizer(wx.VERTICAL)
         self.text_ctrl = wx.TextCtrl(panel)
         my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)
@@ -18,12 +17,10 @@ class MyFrame(wx.Frame):
         self.combobox = wx.Choice(panel, choices=availableTypes, pos=(50,50))
         self.combobox.SetSelection(0)
         my_sizer.Add(self.combobox, 0, wx.ALL | wx.CENTER, 60)
-        self.Bind(wx.EVT_CHOICE, self.OnCombo)
         panel.SetSizer(my_sizer)
         self.Show()
 
-    def OnCombo(self, event):
-        print("Debug")
+
 
     def on_press(self, event):
         ydl_opts_audio = {
@@ -41,14 +38,15 @@ class MyFrame(wx.Frame):
 
         value = self.text_ctrl.GetValue()
         if not value:
-            nothingEntered = wx.MessageBox('Nothing was entered in the box', 'Error', wx.OK)
+            wx.MessageBox('Nothing was entered in the box', 'Error', wx.OK)
         else:
             print("Downloading and converting. Be patient.")
-            userChoice = self.combobox.GetValue()
-            if userChoice == "mp3":
+            userChoice = self.combobox.GetSelection()
+            print(userChoice)
+            if userChoice == 0:
                 with youtube_dl.YoutubeDL(ydl_opts_audio) as ydl:
                     ydl.download([value])
-            elif userChoice == "video":
+            elif userChoice == 1:
                 with youtube_dl.YoutubeDL(ydl_opts_video) as ydl:
                     ydl.download([value])
             resp = wx.MessageBox('Download has finished, are you done?', 'Finished!', wx.YES_NO)
