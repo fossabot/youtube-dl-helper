fc893f265a1fa6af6897f9dc7d9309ff041b9d55
JWBN <admin@wbnk.net>
2020-05-24 13:49:53 +0100

    added directory picker so users can save anywhere. works with all output types

--- main.py
+++ main.py
@@ -14,19 +14,24 @@ class HelperFrame(wx.Frame):
         self.status_label = wx.StaticText(panel, label="Waiting for user input...")
         form_sizer.Add(self.status_label, 0, wx.ALL | wx.TOP, 5)
         form_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)
+        self.directory_output = wx.DirPickerCtrl(panel)
+        form_sizer.Add(self.directory_output, 0, wx.ALL | wx.CENTER, 4)
+
         self.format_selection = wx.Choice(panel, choices=available_formats, pos=(50, 10))
         self.format_selection.SetSelection(1)
         self.quality_selection = wx.Choice(panel, choices=quality_formats, pos=(50, 15))
         self.quality_selection.SetSelection(5)
         self.subtitle_box = wx.CheckBox(panel, label="Subtitles?", pos=(50, 12))
-        form_sizer.Add(self.format_selection, 0, wx.ALL | wx.CENTER, 7)
-        form_sizer.Add(self.subtitle_box, 0, wx.ALL | wx.CENTER, 7)
-        form_sizer.Add(self.quality_selection, 0, wx.ALL | wx.CENTER, 7)
+        form_sizer.Add(self.format_selection, 0, wx.ALL | wx.CENTER, 4)
+        form_sizer.Add(self.subtitle_box, 0, wx.ALL | wx.CENTER, 4)
+        form_sizer.Add(self.quality_selection, 0, wx.ALL | wx.CENTER, 4)
         download_button = wx.Button(panel, label='Download')
         download_button.Bind(wx.EVT_BUTTON, self.on_press)
-        form_sizer.Add(download_button, 0, wx.ALL | wx.CENTER, 5)
+        form_sizer.Add(download_button, 0, wx.ALL | wx.CENTER, 4)
         panel.SetSizer(form_sizer)
         self.Show()
+
+
 
     def download_hook(self, d):
         if d['status'] == 'downloading':
@@ -36,10 +41,17 @@ class HelperFrame(wx.Frame):
             self.status_label.SetLabel("Converting")
 
     def on_press(self, _event_):
+        file_output = self.directory_output.GetPath()
+        if file_output != "":
+            file_output = file_output + "/%(title)s.%(ext)s"
+        else:
+            file_output = "%(title)s.%(ext)s"
+
         quality_choice = self.quality_selection.GetSelection()
         quality_selection = ["144", "240", "360", "480", "720", "1080"]
         ydl_opts_audio = {
             'format': 'bestaudio/best',
+            'outtmpl': file_output,
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
@@ -51,12 +63,14 @@ class HelperFrame(wx.Frame):
         ydl_opts_video_audio = {
             'format': 'bestvideo[height<={}]+bestaudio'.format(quality_selection[quality_choice]),
             'writesubtitles': self.subtitle_box.IsChecked(),
+            'outtmpl': file_output,
             'progress_hooks': [self.download_hook]
         }
 
         ydl_opts_video_noaudio = {
             'format': 'bestvideo[height<={}]'.format(quality_selection[quality_choice]),
             'writesubtitles': self.subtitle_box.IsChecked(),
+            'outtmpl': file_output,
             'progress_hooks': [self.download_hook]
 
         }
