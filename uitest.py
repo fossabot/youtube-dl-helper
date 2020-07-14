b81503ea8bf699846f7c4991697626e01fb9b639
jwbn <jwbn@dropmail.xyz>
2020-07-14 13:24:36 +0100

    new functional gui (with pysimplegui). this addresses issues #14 and #13

--- uitest.py
+++ uitest.py
@@ -1,15 +1,94 @@
+from __future__ import unicode_literals
 import PySimpleGUI as sg
+import youtube_dl
 
-print("Successfully loaded library")
 
-window_layout = [[sg.Text('ytdl-helper')], [sg.Button("OK")]]
 
-window = sg.Window('ytdl-helper', window_layout)
+essential_options = [
+
+    [
+
+        sg.Text("Download Directory:"),
+
+        sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
+
+        sg.FolderBrowse(),
+
+    ],
+
+    [
+
+        sg.In('URL', size=(25, 1), enable_events=True, key="-DLURL-"),
+        sg.Button("Download")
+
+    ],
+
+]
+
+
+
+optional_options = [
+
+    [sg.Text("Not functioning currently")],
+
+
+
+
+
+]
+
+
+# ----- Full layout -----
+
+layout = [
+
+    [
+
+        sg.Column(essential_options),
+
+        sg.VSeperator(),
+
+        sg.Column(optional_options),
+
+    ]
+
+]
+
+
+window = sg.Window("ytdl-helper", layout)
+
+
 
 while True:
+
     event, values = window.read()
-    if event == "OK" or event == sg.WIN_CLOSED:
+
+    if event == "Exit" or event == sg.WIN_CLOSED:
+
         break
+    if event == "Download":
+        video_link = values["-DLURL-"]
+        print(video_link)
+        file_output_directory = values["-FOLDER-"]
+        if not file_output_directory:
+            file_output_directory = "%(title)s.%(ext)s"
+        else:
+            file_output_directory = file_output_directory + "/%(title)s.%(ext)s"
+        dl_opts = {
+            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4',
+            'outtmpl': file_output_directory
+        }
+        if not video_link:
+            sg.Popup('No link', 'No valid link entered.')
+        else:
+            try:
+                with youtube_dl.YoutubeDL(dl_opts) as ydl:
+                    ydl.download([video_link])
+                sg.Popup("Done!", "Video successfully downloaded.")
+            except youtube_dl.utils.DownloadError as download_error:
+                sg.Popup("Error whilst downloading", "Something went wrong! Check your link and try again.")
+
+
 
 
 window.close()
