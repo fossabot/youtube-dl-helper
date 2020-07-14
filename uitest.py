e7f297d1c4cb82a9918e4d2132926c99afb4c360
jwbn <jwbn@dropmail.xyz>
2020-07-14 12:24:49 +0100

    ui testing for rewrite - 2

--- uitest.py
+++ uitest.py
@@ -1,3 +1,15 @@
 import PySimpleGUI as sg
 
 print("Successfully loaded library")
+
+window_layout = [[sg.Text('ytdl-helper')], [sg.Button("OK")]]
+
+window = sg.Window('ytdl-helper', window_layout)
+
+while True:
+    event, values = window.read()
+    if event == "OK" or event == sg.WIN_CLOSED:
+        break
+
+
+window.close()
