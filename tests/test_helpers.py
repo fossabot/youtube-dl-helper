d7712366dafd6d90a5aeb3b2c3d08f78df07f312
jwbn <jwbn@dropmail.xyz>
2020-07-24 20:09:23 +0100

    separate tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -2,9 +2,10 @@ from unittest import TestCase
 import helpers
 
 class Test(TestCase):
-    def test_calculate_directory(self):
+    def test_calculate_empty_directory(self):
         directory = helpers.calculate_directory("")
-        valid_directory = helpers.calculate_directory("C:/Users/Test/Documents")
         self.assertEqual(directory, "%(title)s.%(ext)s")
-        self.assertEqual(valid_directory, "C:/Users/Test/Documents/%(title)s.%(ext)s")
 
+    def test_calculate_valid_directory(self):
+        valid_directory = helpers.calculate_directory("C:/Users/Test/Documents")
+        self.assertEqual(valid_directory, "C:/Users/Test/Documents/%(title)s.%(ext)s")
