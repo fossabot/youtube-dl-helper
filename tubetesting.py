2929a6d1f7942ec3561fca22b8513149788acf55
jwbn <jwbn@noisymail.com>
2020-08-27 13:36:19 +0100

    pytube vid testing (postfix)

--- tubetesting.py
+++ tubetesting.py
@@ -1,12 +1,24 @@
+# pytube test kit
+
+import pytube
 from pytube import YouTube
 print("PyTube loaded")
 
-video = YouTube('https://youtube.com/watch?v=x')
 
+# Progressive (720p and below audio + vid), adaptive (1080p+noaudio)
+video_url = input("Enter video url: ")
+desired_resolution = input("Enter res: ")
+try:
+    video = YouTube(video_url)
+    print(f"Video found. Title: {video.title}")
 
-print(f'Title of video is {video.title}')
+except pytube.exceptions.RegexMatchError as invalid_url_error:
+    print("You entered an invalid URL!")
 
-download_object = video.streams.filter(resolution="1080p", progressive=True).first()
-
-if not download_object:
-    print("Cannot find audio+video stream")
+#
+#download_object = video.streams.filter(resolution="720p", progressive=True).first()
+#
+#if not download_object:
+#    print("Cannot find audio+video stream")
+#else:
+#    print("Found audio+video stream")
