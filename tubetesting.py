8ab5e21a6a0444cb947cc44dd45a3c0d66a16e4f
jwbn <jwbn@noisymail.com>
2020-08-27 17:37:01 +0100

    remove source files automatically. assign temporary files a name with a time stamp

--- tubetesting.py
+++ tubetesting.py
@@ -3,18 +3,27 @@
 import pytube
 from pytube import YouTube
 from pyffmpeg import FFmpeg
+import os
+import time
+
+
+current_time = int(time.time())
 ff = FFmpeg()
-video = YouTube("https://www.youtube.com/watch?v=X")
-user_dir = "C:/Users/X/Documents/X"
+video = YouTube("x")
+user_dir = "x"
 audio_object = video.streams.filter(only_audio=True, mime_type="audio/mp4").first()
 
 
 video_object = video.streams.filter(resolution="1080p", adaptive=True, file_extension="mp4").first()
 print("DLING audio")
-audio_object.download(filename="audio")
+audio_object.download(filename=f'audio-{current_time}')
 print("DLING video")
-video_object.download(filename="video")
+video_object.download(filename=f'video-{current_time}')
 
 print("Preparing conversion")
-ff.options(f'-i audio.mp4 -i video.mp4 -acodec copy -vcodec copy {user_dir}/outputfile.mp4')
-print("Done?")
+ff.options(f'-i audio-{current_time}.mp4 -i video-{current_time}.mp4 -acodec copy -vcodec copy {user_dir}/testfile-{current_time}.mkv')
+print("Finished conversion")
+time.sleep(1)
+os.remove(f'audio-{current_time}.mp4')
+os.remove(f'video-{current_time}.mp4')
+
